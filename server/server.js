9a00a5e3175110bd3322582a8f2a130515903ccb
Om Kale <okofficial012@gmail.com>
2026-02-18 02:07:41 +0530

    fix: use render env vars directly

--- server/server.js
+++ server/server.js
@@ -1,11 +1,9 @@
 const dotenv = require("dotenv");
-dotenv.config({ path: "./config.env" });
+dotenv.config();
 const db = require("./config/dbConfig");
 const server = require("./app");
 
-
-const port = process.env.PORT_NO || 3000;
-
+const port = process.env.PORT || 3000;
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
